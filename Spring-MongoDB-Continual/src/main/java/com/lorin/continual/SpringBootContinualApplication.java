a3bccc4a50fb89f5b250621a38de4de6ad538608
Lorin-Dan <blm027.msi@gmail.com>
2020-05-24 16:36:36 +0300

    Class diagrams

--- Spring-MongoDB-Continual/src/main/java/com/lorin/continual/SpringBootContinualApplication.java
+++ Spring-MongoDB-Continual/src/main/java/com/lorin/continual/SpringBootContinualApplication.java
@@ -3,6 +3,11 @@ package com.lorin.continual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Continual home project Map Calibration
+ * @author lorin
+ *
+ */
 @SpringBootApplication
 public class SpringBootContinualApplication {
 
